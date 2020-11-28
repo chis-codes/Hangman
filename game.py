288a3e7f7fe3b051ad5c326412dbf4fd4d3b35c8
Chris <chrisllamas2000@gmail.com>
2020-11-28 14:08:25 -0800

    Added documentation and added win for guessing all letters, not necessarily full word

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-# Hangman v1
+# Hangman v1.0
 #      ------\
 #      |     |
 #    (o_o)   |
@@ -6,23 +6,16 @@
 #    /   \   |
 #           ===
 # Simple command-line hangman game with randomly generated word from list created by me.
-#    ~ ------\
-# ~      '   |
-# '   `   `  |'
-#   \(^O^)/  |
-#    [   ]   |
-#    /   \  ===
-
 
 import wordGenerator
 import time
 
 # game variables
-word = wordGenerator.gen()
-win = False
-attempts = 0
-user = ""
-guesses = ""
+word = wordGenerator.gen()  # randomly-generated word
+win = False                 # True if player wins
+attempts = 0                # number of failed attempts, if 6, player loses
+user = ""                   # string input given from letters guessed
+guesses = ""                # string holding all characters that the user has guessed
 hangmans = [
     [
         "       ------\ ",
@@ -99,19 +92,28 @@ def checkForLetter(w, char):
             return True
     return False
 
+# checkFullWord
+# iterates through secret word and verifies that letter is in guesses,
+# if all letters are found in guesses, then returns true
+def checkFullWord():
+    for c in word:
+        if checkForLetter(guesses, c) == False:
+            return False # at least 1 letter does not exist, so player has not won yet
+    return True # no error found, return True
+
 # printStatus
 # prints out the current state of guessed word with pool of guessed
 # letters following
 def printStatus():
-    for s in hangmans[attempts]:
+    for s in hangmans[attempts]:    # print the status of the hangman
         print(s)
-    for c in word.lower():
+    for c in word.lower():  # print the secret word with guessed letters filled in
         if checkForLetter(guesses,c) == True:
             print(c, end = " ")
         else:
             print("_", end = " ")
     print("")
-    if(len(guesses)>0):
+    if(len(guesses)>0):     # print all letters incorrectly guessed
         print("Pool:", end = " ")
         for c in guesses.lower():
             if checkForLetter(word,c) == False:
@@ -119,7 +121,6 @@ def printStatus():
         print("")
 
 # output prompt
-print("")
 print("============================")
 print("Let's play hangman!")
 print("============================")
@@ -127,6 +128,7 @@ print("Guess a letter or the word when you're ready!")
 print("To quit anytime, just enter 'quit()'")
 print("--")
 
+# run game until 'quit()' or loss or win
 while user != "quit()":
     if user != "":
         if len(user)>1:
@@ -140,32 +142,46 @@ while user != "quit()":
                 print("Not quite!")
                 time.sleep(1)
         else:
-            guesses+=user
-            if checkForLetter(word, user) == True:
-                print("Correct!")
-                time.sleep(1)
+            if checkForLetter(guesses, user) == False:  # avoid duplicate inputs
+                guesses+=user
+                if checkForLetter(word, user) == True:
+                    if checkFullWord() == True:
+                        win = True
+                        break
+                    print("Yes!")
+                    time.sleep(1)
+                else:
+                    attempts+=1
+                    if(attempts==6): break
+                    print("Try again!")
+                    time.sleep(1)
             else:
-                attempts+=1
-                if(attempts==6): break
-                print("Try again!")
-                time.sleep(1)
+                if checkForLetter(word, user) == True:
+                    print("Correct, but you guessed this already!")
+                    time.sleep(1)
+                else:
+                    attempts+=1
+                    if(attempts==6): break
+                    print("Careful!  You already guessed this wrong letter!")
+                    time.sleep(1)
         print("")
     printStatus()
     user = input("> ")
 
+# final print
 if win == True:
     print("")
-    for s in hangmans[7]:
+    for s in hangmans[7]:   # print hangman (win emote)
         print(s)
-    for c in word:
+    for c in word:          # print revealed secret word
         print(c, end=" ")
     print("")
     print("Hooray!  Thanks for playing!")
 else:
     print("")
-    for s in hangmans[6]:
+    for s in hangmans[6]:   # print hangman (loss emote)
         print(s)
-    for c in word:
+    for c in word:          # print revealed secret word
         print(c, end=" ")
     print("")
     print("Better luck next time.  Thanks for playing!")
