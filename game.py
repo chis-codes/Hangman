46f997bbfce693941e221e37c6f87f5f2887f8c6
Chris <chrisllamas2000@gmail.com>
2020-11-28 00:58:07 -0800

    added checkForLetter()

---
+++ game.py
@@ -0,0 +1,11 @@
+import string 
+
+# checkForLetter
+# iterates through 'word' to check if the letter 'char' exists
+# returns true if exists, else false
+def checkForLetter(word, char):
+    for c in word.lower():
+        if c == char.lower():
+            return True
+    return False
+
---
+++ wordGenerator.py
@@ -0,0 +1,11 @@
+import random
+
+words = [
+    "apple",
+    "banana",
+    "coconut",
+    "diamond"
+]
+
+def gen(): 
+    r = random.randrange(0,len(words),1)
